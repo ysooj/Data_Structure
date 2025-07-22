55b0968375b2327b8436fb3ba889fe6ce2778319
ysooj <musukie52@gmail.com>
2025-07-22 14:39:08 +0900

    [과제] (2025.07.22) vector 구조 정의

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -11,18 +11,22 @@ template <typename T>
 class Vector
 {
 private:
-    int count;
-    int capacity;
+    int count;      // 현재 저장된 원소의 개수
+    int capacity;   // 할당된 메모리 공간의 크기 (즉, 배열의 최대 크기)
 
-    T * array;
+    T * array;      // 실제 데이터를 저장할 동적 배열의 포인터  
 
 public:
     Vector()
     {
-        count = 0;
-        capacity = 1;
+        // 초기화
+        count = 0;                  // 아직 아무 원소도 없음
+        capacity = 1;               // 1개의 원소 저장 가능
 
-        array = T[capacity];
+        array = new T[capacity];    // 동적 배열 할당
+        // 타입 T의 연속된 메모리 공간을 capacity만큼 힙(heap)에 할당한 것.
+        // 동적 배열을 만들려면 몇 개의 요소를 저장할지를 미리 알려줘야 한다.
+        // 그래야 운영체제가 그만큼의 연속된 메모리 공간을 할당할 수 있다.
     }
 };
 
