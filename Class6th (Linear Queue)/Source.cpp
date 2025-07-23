b92a7946ab92f8626c2adce0291652fd7585ba28
ysooj <musukie52@gmail.com>
2025-07-23 21:44:08 +0900

    (2025.7.23) 선형 큐

--- Class6th (Linear Queue)/Source.cpp
+++ Class6th (Linear Queue)/Source.cpp
@@ -1,20 +1,119 @@
-﻿// Class6th (Linear Queue).cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
 
-#include <iostream>
+#define SIZE 5
+
+using namespace std;
+
+// --------------------------------------------------------
+// [Queue]
+// 선형 큐. Linear
+// 정적 배열
+// 먼저 넣은 것이 먼저 나오는 구조. (FIFO: First In, First Out)
+// --------------------------------------------------------
+
+template <typename T>
+
+class Queue
+{
+private:
+    int rear;
+    int front;
+
+    T container[SIZE];
+
+public:
+    Queue()
+    {
+        rear = 0;
+        front = 0;
+
+        for (int i = 0; i < SIZE; i++)
+        {
+            container[i] = NULL;
+        }
+    }
+
+    void push(T data)
+    {
+        // 다 차면 오버플로우 문구 출력
+        if (rear >= SIZE)
+        {
+            cout << "Linear queue overflow" << endl;
+        }
+
+        // data 넣고 rear 값을 증가
+        else
+        {
+            container[rear++] = data;
+        }
+
+    }
+
+    void pop()
+    {
+        if (empty())
+        {
+            cout << "Linear queue is empty" << endl;
+        }
+
+        else
+        {
+            container[front++] = NULL;
+        }
+    }
+
+    const int& size()
+    {
+        return rear - front;
+    }
+
+    const bool& empty()
+    {
+        return front == rear;
+    }
+
+    // 가장 앞에 있는 데이터를 출력할 때는 peek이라는 함수명을 사용한다.(front보다는 peek을 씀)
+    const T& peek()
+    {
+        // 비어있으면 exit(1) 해준다.
+        if (empty())
+        {
+            exit(1);
+        }
+
+        else
+        {
+            return container[front];
+        }
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Queue <int> queue;
+
+    queue.push(10);
+    queue.push(20);
+    queue.push(30);
+    queue.push(40);
+    queue.push(50);
+    queue.push(100000);
+    queue.empty();
+
+    cout << queue.size() << endl;
+
+    while (queue.empty() == false)
+    {
+        cout << queue.peek() << endl;
+        queue.pop();
+
+        cout << "----------------" << endl;
+    }
+
+    cout << queue.size() << endl;
+    queue.empty();
+
+    return 0;
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+// stack이 비워진 다음에 다시 data를 넣으려 하면 터진다. 왜 그런 지는 집 가서 다시 영상으로 확인하자.
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,12 +1,12 @@
 ﻿#include <iostream>
 
-#define SIZE 5
+#define SIZE 4
 
 using namespace std;
 
 // --------------------------------------------------------
 // [Queue]
-// 선형 큐. Linear
+// 원형 큐. Linear
 // 정적 배열
 // 먼저 넣은 것이 먼저 나오는 구조. (FIFO: First In, First Out)
 // --------------------------------------------------------
@@ -24,8 +24,8 @@ private:
 public:
     Queue()
     {
-        rear = 0;
-        front = 0;
+        rear = SIZE - 1;
+        front = SIZE - 1;
 
         for (int i = 0; i < SIZE; i++)
         {
@@ -35,83 +35,23 @@ public:
 
     void push(T data)
     {
-        // 다 차면 오버플로우 문구 출력
-        if (rear >= SIZE)
+        if ((rear + 1) % SIZE == front)
         {
-            cout << "Linear queue overflow" << endl;
-        }
-
-        // data 넣고 rear 값을 증가
-        else
-        {
-            container[rear++] = data;
-        }
-
-    }
-
-    void pop()
-    {
-        if (empty())
-        {
-            cout << "Linear queue is empty" << endl;
+            cout << "Circular Queue overflow" << endl;
         }
 
         else
         {
-            container[front++] = NULL;
+            rear = (rear + 1) % SIZE;
+            container[rear] = data;
         }
     }
 
-    const int & size()
-    {
-        return rear - front;
-    }
-
-    const bool & empty()
-    {
-        return front == rear;
-    }
-
-    // 가장 앞에 있는 데이터를 출력할 때는 peek이라는 함수명을 사용한다.(front보다는 peek을 씀)
-    const T & peek()
-    {
-        // 비어있으면 exit(1) 해준다.
-        if (empty())
-        {
-            exit(1);
-        }
-        
-        else
-        {
-            return container[front];
-        }
-    }
 };
 
 int main()
 {
-    Queue <int> queue;
-
-    queue.push(10);
-    queue.push(20);
-    queue.push(30);
-    queue.push(40);
-    queue.push(50);
-    queue.push(100000);
-    queue.empty();
-
-    cout << queue.size() << endl;
-
-    while (queue.empty() == false)
-    {
-        cout << queue.peek() << endl;
-        queue.pop();
-
-        cout << "----------------" << endl;
-    }
-
-    cout << queue.size() << endl;
-    queue.empty();
+   
 
     return 0;
 }
