da9a3f4f54fbb9b0fe4f8c82b3f0fe786c88fc0b
ysooj <musukie52@gmail.com>
2025-07-24 21:43:40 +0900

    (2025.7.24) 원형 큐

---
+++ Class7th (Circle Queue)/Source.cpp
@@ -0,0 +1,115 @@
+﻿#include <iostream>
+
+#define SIZE 4  // 큐의 최대 크기 정의
+
+using namespace std;
+
+// --------------------------------------------------------
+// [Circular Queue] ; 원형 큐 구현
+// 정적 배열을 사용하여 고정 크기의 큐 생성
+// 먼저 넣은 데이터가 먼저 나오는 구조 (FIFO: First In, First Out)
+// 하나를 비워놓는다. 이유는
+// front와 rear가 같을 때 큐가 비어있는지 또는 가득 찼는지를 구분하기 위해서이다.
+// 즉, 큐가 가득 찬 상태와 비어있는 상태를 구분하려면 항상 한 칸을 비워두어야 한다.
+// --------------------------------------------------------
+
+template <typename T>
+
+class Queue
+{
+private:
+    int rear;           // 데이터가 마지막으로 들어간 위치
+    int front;          // 데이터가 처음으로 나오는 위치
+
+    T container[SIZE];  // 큐를 저장할 배열
+
+public:
+    Queue()
+    {
+        rear = SIZE - 1;
+        front = SIZE - 1;
+
+        for (int i = 0; i < SIZE; i++)
+        {
+            container[i] = NULL;
+        }
+    }
+
+    void push(T data)
+    {
+        // 큐가 가득 찼는지 확인 (원형 큐의 조건)
+        if (front == (rear + 1) % SIZE)
+            // (rear + 1) % SIZE  =>  현재 rear 위치에서 한 칸 앞으로 이동한 인덱스를 구하되, 배열 범위를 넘으면 다시 0부터 시작하도록 순환시킨다는 뜻
+        {
+            cout << "Circular Queue overflow" << endl;
+        }
+
+        else
+        {
+            // rear를 다음 위치로 이동시키고 데이터 삽입
+            rear = (rear + 1) % SIZE;
+            container[rear] = data;
+        }
+    }
+
+    void pop()
+    {
+        if (empty())
+        {
+            cout << "Circular Queue is empty" << endl;
+        }
+
+        else
+        {
+            front = (front + 1) % SIZE;
+
+            container[front] = NULL;
+        }
+    }
+
+    const bool& empty()
+    {
+        return front == rear;
+    }
+
+    const T& peek()
+    {
+        if (empty())
+        {
+            exit(1);    // 큐가 비어 있으면 프로그램을 강제 종료
+        }
+        else
+        {
+            return container[(front + 1) % SIZE];
+        }
+    }
+};
+
+int main()
+{
+    Queue <int> queue;
+
+    queue.push(10);
+    queue.push(20);
+    queue.push(30);
+    queue.push(40);
+
+    cout << queue.empty() << endl;
+
+    while (queue.empty() == false)
+    {
+        cout << queue.peek() << endl;
+        queue.pop();
+        cout << "---------------" << endl;
+    }
+
+    cout << queue.empty() << endl;
+
+    queue.push(50);
+    queue.push(60);
+    queue.push(70);
+
+    cout << queue.peek() << endl;
+
+    return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,60 +1,138 @@
 ﻿#include <iostream>
 
-#define SIZE 4  // 큐의 최대 크기 정의
-
 using namespace std;
 
 // --------------------------------------------------------
-// [Queue]
-// 원형 큐 (Circular Queue)의 구현
-// 정적 배열을 사용하여 고정 크기의 큐 생성
-// 먼저 넣은 데이터가 먼저 나오는 구조 (FIFO: First In, First Out)
+// [Priority Queue] ; 우선순위 큐
+// 일반적인 큐(FIFO)와는 달리, 요소들이 우선순위에 따라 처리됨
+// 가장 높은 우선순위를 가진 요소가 가장 먼저 나감
+// 내부적으로 보통 힙(Heap)을 사용하여 구현 (최대 힙 또는 최소 힙)
+// 부모 노드의 우선순위가 자식 노드보다 높도록 구성
+// 동적 배열(또는 벡터)을 통해 힙 트리를 구성할 수 있음
 // --------------------------------------------------------
 
 template <typename T>
 
-class Queue
+class PriorityQueue
 {
 private:
-    int rear;           // 데이터가 마지막으로 들어간 위치
-    int front;          // 데이터가 처음으로 나오는 위치
+    int index;
+    int capacity;
 
-    T container[SIZE];  // 큐를 저장할 배열
-
+    T * container;
 public:
-    Queue()
+    PriorityQueue()
     {
-        rear = SIZE - 1;
-        front = SIZE - 1;
+        index = 0;
+        capacity = 0;
+        container = nullptr;
+    }
 
-        for (int i = 0; i < SIZE; i++)
+    void resize(int newSize)
+    {
+        // 1. capacity에  새로운 size 값을 저장합니다.
+        capacity = newSize;
+
+        // 2. 새로운 포인터 변수를 생성해서 새롭게 만들어진 메모리 공간을 가리키도록 합니다.
+        T* temporary = new T[capacity];
+
+        // 3. 새로운 메모리 공간의 값을 초기화합니다.
+        for (int i = 0; i < capacity; i++)
         {
-            container[i] = NULL;
+            temporary[i] = NULL;
         }
+
+        // 4. 기존 배열에 있는 값을 복사해서 새로운 배열에 넣어줍니다.
+        for (int i = 0; i < index; i++)
+        {
+            temporary[i] = container[i];
+        }
+
+        //5. 기존 배열의 메모리를 해제합니다. (if문을 쓰지 않고 그냥 'delete [] container;' 이라고 해도 된다고 한다.)
+        if (container != nullptr)
+        {
+            delete[] container;
+        }
+
+        // 6. 기존에 배열을 가리키던 포인터 변수의 값을 새로운 배열의 시작 주소로 가리킵니다.
+        container = temporary;
     }
 
     void push(T data)
     {
-        // 큐가 가득 찼는지 확인 (원형 큐의 조건)
-        if ((rear + 1) % SIZE == front)
-        // (rear + 1) % SIZE  =>  현재 rear 위치에서 한 칸 앞으로 이동한 인덱스를 구하되, 배열 범위를 넘으면 다시 0부터 시작하도록 순환시킨다는 뜻
+        if (capacity <= 0)
         {
-            cout << "Circular Queue overflow" << endl;
+            resize(1);
         }
 
-        else
+        else if (index >= capacity)
         {
-            // rear를 다음 위치로 이동시키고 데이터 삽입
-            rear = (rear + 1) % SIZE;
-            container[rear] = data;
+            resize(capacity * 2);
+        }
+
+        container[index++] = data;
+
+        int child = index - 1;
+        int parent = (child - 1) / 2;
+
+        while (child > 0)
+        {
+
+            if (container[child] > container[parent])
+            {
+                std::swap(container[child], container[parent]);
+            }
+            child = parent;
+            parent = (child - 1) / 2;
         }
     }
 
+    void pop()
+    {
+
+    }
+
+    const bool & empty()
+    {
+        return index <= 0;
+    }
+
+    const int & size()
+    {
+        return index;
+    }
+
+    const T & top()
+    {
+        if (empty())
+        {
+            exit(1);
+        }
+        else
+        {
+            return container[0];
+        }
+    }
+
+    ~PriorityQueue()
+    {
+        if (container != nullptr)
+        {
+            delete [] container;
+        }
+    }
 };
 
 int main()
 {
-   
+    PriorityQueue <int> priorityQueue;
+
+    priorityQueue.push(17);
+    priorityQueue.push(31);
+    priorityQueue.push(9);
+    priorityQueue.push(15);
+
+    cout << priorityQueue.top() << endl;
 
     return 0;
 }
