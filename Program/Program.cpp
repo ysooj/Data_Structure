962218a17d18beccf7fbdf6fc3c13ef88a095d24
ysooj <musukie52@gmail.com>
2025-07-29 13:58:54 +0900

    [과제](2025.7.29) erase 함수 구현(+생성자 수정)

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -42,6 +42,11 @@ public:
         // bucket => Bucket 구조체 배열을 가리키는 포인터
         // bucket[i] => i번째 Bucket 구조체(실제 객체, 포인터 아님)
         // bucket[i].head => i번째 버킷의 연결 리스트 시작 노드
+        for (int i = 0; i < size; i++)
+        {
+            bucket[i].head = nullptr;
+            bucket[i].count = 0;
+        }
     }
 
     unsigned int hash_function(KEY key)  // hashtable의 key는 size 값보다 작아야 하므로.
@@ -79,6 +84,7 @@ public:
         bucket[hashIndex].count++;
     }
 
+    // 처음으로 발견한 key만 삭제하는 함수
     void erase(KEY key)
     {
         // 1. hash_function 함수를 통해서 값을 받는 임시 변수
@@ -91,59 +97,94 @@ public:
         Node* previousNode = nullptr;
 
         // 4. currentNode가 nullptr과 같다면(해당하는 key에 대해 지울 것이 없는 경우) 함수를 종료합니다.
-        if (currentNode == nullptr)
+        while (currentNode != nullptr)
         {
-            cout << "Not Key Found" << endl;
-            return;
-        }
-
-        // 5. 해당하는 key에 대해 지울 것이 있는 경우
-        else
-        {
-            while (currentNode != nullptr)
+            // 5. 해당하는 key에 대해 지울 것이 있는 경우
+            // [문자열의 주소값만 비교]
+            // if (currentNode->key == key)
+            // [문자열 내용 비교]
+            if (currentNode->key == key)
             {
-                // key 값을 찾지 못했다면
-                // previousNode를 currentNode로 하고 currentNode는 다음으로 넘겨준다.
-                if (currentNode->key != key)
+                // 지울 게 head라면
+                // head는 currentNode->next가 되며, currentNode를 지운다.
+                if (previousNode == nullptr)
                 {
-                    previousNode = currentNode;
-                    currentNode = currentNode->next;
+                    bucket[hashIndex].head = currentNode->next;
                 }
-
                 // key 값을 찾았다면
                 // previousNode->next는 currentNode->next가 되며, currentNode를 지운다.
                 else
                 {
-
-                }
-                if (currentNode->key == key)
-                {
                     previousNode->next = currentNode->next;
-
-                    delete currentNode;
-
-                    bucket[hashIndex].count--;
                 }
 
-                // 지울 게 head라면
-                // head는 currentNode->next가 되며, currentNode를 지운다.
-                else if (previousNode == nullptr;)
+                delete currentNode;
+                bucket[hashIndex].count--;
+
+                cout << "Key erased : " << key << endl;
+                return;  // 삭제 후 함수 종료
+            }
+
+            // key 값을 찾지 못했다면
+            // previousNode를 currentNode로 하고 currentNode는 다음으로 넘겨준다.
+            previousNode = currentNode;
+            currentNode = currentNode->next;
+        }
+
+        // 다 살펴봤는 데도 key를 못 찾았다면
+        // key를 찾지 못했다는 문구를 출력한다.
+        // 여기까지 왔다는 건 key를 못 찾았다는 뜻
+        cout << "Not Key Found" << endl;
+        return;
+    }
+
+    // 중복되는 key를 모두 삭제하는 함수
+    void erase_all(KEY key)
+    {
+        int hashIndex = hash_function(key);
+        Node * currentNode = bucket[hashIndex].head;
+        Node * previousNode = nullptr;
+        
+        while (currentNode != nullptr)
+        {
+            if (currentNode->key == key)  // 문자열 비교
+            {
+                Node * nextNode = currentNode->next;
+
+                if (previousNode == nullptr)
                 {
-                    bucket[hashIndex].head = currentNode->next;
-
-                    delete currentNode;
-
-                    bucket[hashIndex].count--;
+                    bucket[hashIndex].head = nextNode;
                 }
-
-                // 다 살펴봤는 데도 key를 못 찾았다면
-                // key를 찾지 못했다는 문구를 출력한다.
                 else
                 {
-                    cout << "Not Key Found" << endl;
-                    break;
+                    previousNode->next = nextNode;
                 }
+
+                delete currentNode;
+                bucket[hashIndex].count--;
+
+                currentNode = nextNode;
+
+                // 해당하는 key를 삭제할 때마다 메시지 출력
+                cout << "Key Deleted : " << key << endl;
             }
+
+            else
+            {
+                previousNode = currentNode;
+                currentNode = currentNode->next;
+            }
+        }
+
+        // 모든 노드가 삭제되어 버킷이 빈 상태
+        if (bucket[hashIndex].head == nullptr || bucket[hashIndex].count == 0)
+        {
+            cout << "All matching keys deleted. Bucket is now empty." << endl;
+        }
+        // 삭제된 노드가 하나도 없는 경우
+        else if (bucket[hashIndex].head != nullptr)
+        {
+            cout << "Not Key Found" << endl;
         }
     }
 
@@ -167,6 +208,18 @@ int main()
     hashtable.insert("Abyssal Mask", 3000);
     hashtable.insert("Bami's Cinder", 1000);
     hashtable.insert("Chain Vest", 800);
+
+    // 삭제 시도 - 존재하는 키
+    hashtable.erase("Bami's Cinder");
+
+    // 삭제 시도 - 존재하지 않는 키
+    hashtable.erase("Nonexistent Key");
+
+    // 중복 key 삽입 후 모두 삭제해보기
+    hashtable.insert("Chain Vest", 900);
+    hashtable.insert("Chain Vest", 850);
+    
+    hashtable.erase_all("Chain Vest");
 
     return 0;
 }
