0cfd29b5bfb307fe679ee482b348b67e1c647898
ysooj <musukie52@gmail.com>
2025-08-05 21:43:44 +0900

    (2025.8.5) 이진 탐색 트리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -41,8 +41,13 @@ private:
     struct Node
     {
         T data;
-        Node * left;
-        Node * right;
+        Node * left = nullptr;
+        Node * right = nullptr;
+
+        Node(T data)
+        {
+            this->data = data;
+        }
     };
 
     Node * root;
@@ -53,7 +58,7 @@ public:
         root = nullptr;
     }
 
-    void insert(T data)
+    void insert_me(T data)
     {
         // root가 비어있을 때 : data를 넣고, left와 right에는 nullptr 넣기
         if (root == nullptr)
@@ -69,7 +74,7 @@ public:
         else
         {
             // currentNode가 root를 가리키게 하고
-            Node * currentNode = root;
+            Node* currentNode = root;
 
             // 자식 노드가 있으면 계속 타고 들어가야 한다.
             while (true)
@@ -85,6 +90,8 @@ public:
                         currentNode->left->data = data;
                         currentNode->left->left = nullptr;
                         currentNode->left->right = nullptr;
+
+                        break;  // 삽입 후 함수 종료
                     }
 
                     else
@@ -102,6 +109,8 @@ public:
                         currentNode->right->data = data;
                         currentNode->right->left = nullptr;
                         currentNode->right->right = nullptr;
+
+                        break;  // 삽입 후 함수 종료
                     }
 
                     else
@@ -119,6 +128,188 @@ public:
             }
         }
     }
+
+    void insert(T data)
+    {
+        Node * newNode = new Node(data);
+
+        if (root == nullptr)
+        {
+            root = newNode;
+        }
+
+        else
+        {
+            Node * currentNode = root;
+
+            while (currentNode != nullptr)
+            {
+                if (currentNode->data == data)
+                {
+                    delete newNode;
+
+                    return;
+                }
+
+                else if (currentNode->data > data)
+                {
+                    if (currentNode->left == nullptr)
+                    {
+                        currentNode->left = newNode;
+                        break;
+                    }
+                    else
+                    {
+                        currentNode = currentNode->left;
+                    }
+                }
+
+                else
+                {
+                    if (currentNode->right == nullptr)
+                    {
+                        currentNode->right = newNode;
+                        break;
+                    }
+                    else
+                    {
+                        currentNode = currentNode->right;
+                    }
+                }
+            }
+        }
+    }
+
+    // 후위 순회(Left → Right → Root) ; 후위 순회를 사용해야 자식 노드부터 안전하게 지운 뒤 부모 노드를 삭제할 수 있다.
+    void release(Node * root)
+    {
+        // 1. 현재 노드가 nullptr이 아니면 재귀 함수 호출
+        if (root != nullptr)
+        {
+            // 2. 왼쪽 자식 노드부터 재귀적으로 삭제
+            //    왼쪽 자식이 nullptr이 될 때까지 쭉 들어감
+            release(root->left);
+
+            // 3. 왼쪽 서브트리가 모두 삭제되면 오른쪽 자식 노드도 재귀적으로 삭제
+            //    오른쪽 자식도 nullptr이 될 때까지 쭉 들어감
+            release(root->right);
+
+            // 4. 왼쪽, 오른쪽 자식 노드가 모두 삭제된 후
+            //    현재 노드를 안전하게 삭제
+            delete root;
+        }
+    }
+
+    void erase(T data)
+    {
+        Node * currentNode = root;
+        Node * parentNode = nullptr;
+
+        while (currentNode != nullptr && currentNode->data != data)
+        {
+            if (data < currentNode->data)
+            {
+                parentNode = currentNode;
+                currentNode = currentNode->left;
+            }
+
+            else
+            {
+                parentNode = currentNode;
+                currentNode = currentNode->right;
+            }
+        }
+
+        // erase 함수의 역할
+        // 1. 자식 노드가 하나도 없을 때
+        if (currentNode->left == nullptr && currentNode->right == nullptr)
+        {
+            if (currentNode == root && parentNode == root)
+            {
+                root = nullptr;
+
+                delete currentNode;
+            }
+
+            else
+            {
+                delete currentNode;
+
+                currentNode = parentNode;
+            }
+        }
+
+        cout << "Found the data!" << endl;
+
+
+        
+         
+        
+        // 2. 자식 노드가 하나만 있을 때
+        
+        
+        // 3. 자식 노드가 두 개 있을 때
+    }
+
+    void erase_gpt(T data)
+    {
+        Node* currentNode = root;
+        Node* parentNode = nullptr;
+
+        // 삭제할 노드 탐색
+        while (currentNode != nullptr && currentNode->data != data)
+        {
+            parentNode = currentNode;
+
+            if (data < currentNode->data)
+                currentNode = currentNode->left;
+            else
+                currentNode = currentNode->right;
+        }
+
+        // 찾지 못한 경우
+        if (currentNode == nullptr)
+        {
+            cout << "해당 데이터를 찾을 수 없습니다." << endl;
+            return;
+        }
+
+        // 찾은 경우
+        cout << "찾았다! 삭제할 데이터는: " << currentNode->data << endl;
+
+        // [1] 자식 노드가 하나도 없는 경우 (리프 노드)
+        if (currentNode->left == nullptr && currentNode->right == nullptr)
+        {
+            // (1) 삭제 대상이 루트 노드인 경우
+            if (currentNode == root)
+            {
+                delete root;
+                root = nullptr;
+            }
+            // (2) 부모 노드에서 자식 연결 끊기
+            else if (parentNode->left == currentNode)
+            {
+                delete currentNode;
+                parentNode->left = nullptr;
+            }
+            else if (parentNode->right == currentNode)
+            {
+                delete currentNode;
+                parentNode->right = nullptr;
+            }
+
+            return; // 여기서 끝내야 아래 코드 실행 안 함
+        }
+
+        // [2] 자식 노드가 하나만 있을 때 (작성 예정)
+
+        // [3] 자식 노드가 두 개 있을 때 (작성 예정)
+    }
+
+    ~Set()
+    {
+        release(root);
+    }
 };
 
 int main()
@@ -126,6 +317,10 @@ int main()
     Set <int> set;
 
     set.insert(10);
+    set.insert(6);
+    set.insert(20);
+    set.insert(3);
+    set.insert(15);
 
     return 0;
 }
